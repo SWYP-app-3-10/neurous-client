ddb0d08758eb43a2118272c884d875b6d414c386
Gahyun-313 <gus20313@gmail.com>
2026-06-21 02:16:53 +0900

    feat(commonIcons): level_change_check 아이콘 컴포넌트 export 추가

--- src/icons/commonIcons/commonIcons.tsx
+++ src/icons/commonIcons/commonIcons.tsx
@@ -4,6 +4,7 @@ import {
   createIconComponent,
   createRectangleIconComponent,
 } from '../config/iconUtils';
+import Level_change_check from '../../assets/svg/Level_change_check.svg';
 import Check from '../../assets/svg/Check_.svg';
 import Close from '../../assets/svg/Close_.svg';
 import First from '../../assets/svg/First.svg';
@@ -35,6 +36,10 @@ import P_Icon from '../../assets/svg/P_Icon.svg';
 import XP_Icon from '../../assets/svg/XP_Icon.svg';
 import X_icon from '../../assets/svg/X_icon.svg';
 
+export const LevelChangeCheckIcon = createIconComponent(
+  Level_change_check,
+  scaleWidth(28),
+);
 export const Ic_backIcon = createIconComponent(Icon_back, ICON_SIZES.XL);
 export const CircleIcon = createIconComponent(Circle, scaleWidth(14));
 export const InfoIcon = createIconComponent(Info, ICON_SIZES.L);
