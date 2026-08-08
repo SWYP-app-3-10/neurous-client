3d56929cee4bb710e8f1f7f93e34dac7c7ec66b1
김가현 <gus20313@gmail.com>
2026-08-08 17:07:56 +0900

    fix(ui): 알림 설정 팝업 - 설명 수정

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -242,7 +242,7 @@ const LoginScreen = () => {
           showModal({
             title: '알림을 받으시겠어요?',
             description:
-              '알림을 켜두면, 하루 두 번 문해력 루틴을 \n잊지 않고 챙길 수 있어요!',
+              '하루 두 번 알림으로/n읽기를 잊지 않고 이어갈 수 있어요!ㄴ',
             descriptionColor: COLORS.gray600,
             primaryButton: {
               title: '알림 받을래요',
